31214483c68792558e47976bf76a235a0da8a5c2
ziyu.wang <ziyu.wang@aseit.cn>
2019-03-28 10:08:07 +0800

    第一天二分法查找练习题

--- src/com/aseit/BinarySearch.java
+++ src/com/aseit/BinarySearch.java
@@ -15,7 +15,6 @@ public class BinarySearch {
 
         Scanner scanner = new Scanner(System.in);
         int[] arr = new int[5];
-
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scanner.nextInt();
         }
