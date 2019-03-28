7aed772ec911b21d71a355953e9ffd921a1bbdd2
ziyu.wang <ziyu.wang@aseit.cn>
2019-03-28 10:00:08 +0800

    测试提交

---
+++ src/com/aseit/BinarySearch.java
@@ -0,0 +1,18 @@
+package com.aseit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: @ziyu  freedomziyua@gmail.com
+ * Date: 2019-03-28
+ * Time: 9:59
+ * Description:
+ */
+public class BinarySearch {
+
+    public static void main(String args[]) {
+
+
+    }
+
+
+}
