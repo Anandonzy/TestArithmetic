652c2397f64d5d31805a17ac8d5c0a9e4dc56bb9
ziyu.wang <ziyu.wang@aseit.cn>
2019-04-23 17:34:46 +0800

    更新一下新的写法

--- src/com/aseit/binarySearch/FindMinimum.java
+++ src/com/aseit/binarySearch/FindMinimum.java
@@ -41,4 +41,28 @@ public class FindMinimum {
             return nums[end];
         }
     }
+
+    //提供的比较规范的写法
+    public static int searchAnser(int[] nums) {
+        if (nums.length == 0 || nums == null) {
+            return -1;
+        }
+
+        int start = 0, end = nums.length - 1;
+
+        //这个地方为了防止从第0的位置开始旋转
+        int res = 0;
+        int target = nums[end];
+        while (start <= end) {
+            int mid = start + ((end - start) >> 1);
+            if (nums[mid] < target) {
+                res = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return nums[res];
+    }
+
 }
